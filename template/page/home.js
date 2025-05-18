f62ad523a6066a944828d5abaee5a7eda90acd65
Coding-for-Machine <codingformachine@gmail.com>
2025-05-18 21:07:09 +0500

    my commits

--- template/page/home.js
+++
@@ -1,6 +0,0 @@
-export const HomePage = () => {
-    return `
-      <h1 class="text-3xl font-bold mb-4">Bosh sahifa</h1>
-      <p>Xush kelibsiz! Bu Single Page Application (SPA) misolidir.</p>
-    `;
-  };
